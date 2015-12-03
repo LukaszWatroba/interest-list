c1a9c8695e4f41ef9c6966a697f642526c0d38d9
LukaszWatroba <lukasz.jan.watroba@gmail.com>
2015-12-03 21:00:59 +0100

    lodash helper

--- test/get-data.js
+++ test/get-data.js
@@ -3,8 +3,11 @@
 var test = require('tap').test;
 var interests = require('../')();
 var data = require('../data.json');
+var _ = require('lodash');
 
 test('get all interests', function (t) {
+  // console.log(_.chain(data).sortBy().uniq().value());
+
   t.equal(interests.getData().length, data.length, 'data list should be the same lenght as data.json');
   t.type(interests.getData(), Array, 'data list is an array');
   t.type(interests.getData()[0], 'string', 'first item in list should be string');
